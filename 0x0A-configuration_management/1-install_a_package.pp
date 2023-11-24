c348c79b7cef3fa34a2b2fad0cae916fc356329f
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-11-24 07:21:23 -0800

    change werkzeug version 3.0.1 to 2.2.2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,11 @@
 
 package { 'flask':
   ensure   => '2.1.0',
+  require  => Package['werkzeug'],
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.2.2',
   provider => 'pip3',
 }
