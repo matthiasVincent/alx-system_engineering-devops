4116d7d9dfa28c72186f9c369a0922afa7f0ed24
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2024-01-17 04:15:27 -0800

    second commit

--- 0x16-api_advanced/tests/0-subs.py
+++ 0x16-api_advanced/tests/0-subs.py
@@ -3,19 +3,22 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
+def number_of_subscribers():
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/programming/about.json"
     headers = {
-        "User-Agent": "Api-advanced by matthiasVincent"
+        "User-Agent": "Api-advanced by matthiasVinco"
     }
     data = {
             'grant_type': 'password',
             'username': 'matthiasVinco',
             'password': 'Matthias@28908'
             }
-    response = requests.get(url, data=data, headers=headers, allow_redirects=False)
+    response = requests.get(url, data=data, headers={
+        'User-Agent': 'Api-advanced by matthiasVinco'})
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    results = response.status_code
+    return results
+
+print(number_of_subscribers())
